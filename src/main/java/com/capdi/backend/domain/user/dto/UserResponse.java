aade0b5d6a74b2e13af2b51f9f82d493b23ffeed
KimChaeWon12 <168625140+KimChaeWon12@users.noreply.github.com>
2026-05-14 21:01:45 +0900

    Revert "feat: 사용자 정보 조회 API 구현"

--- src/main/java/com/capdi/backend/domain/user/dto/UserResponse.java
+++ src/main/java/com/capdi/backend/domain/user/dto/UserResponse.java
@@ -1,27 +1,4 @@
 package com.capdi.backend.domain.user.dto;
 
-import com.capdi.backend.domain.user.entity.User;
-import com.capdi.backend.domain.user.entity.UserTypeEnum;
-import lombok.Builder;
-import lombok.Getter;
-
-@Getter
-@Builder
 public class UserResponse {
-
-    private Long userId;
-    private UserTypeEnum userType;
-    private String name;
-    private String email;
-    private String phone;
-
-    public static UserResponse from(User user) {
-        return UserResponse.builder()
-                .userId(user.getId())
-                .userType(user.getUserType())
-                .name(user.getName())
-                .email(user.getEmail())
-                .phone(user.getPhone())
-                .build();
-    }
 }
